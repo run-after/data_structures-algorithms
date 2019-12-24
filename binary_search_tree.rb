8b235374ca6cacb4820657435d186f4d359e5cf2
run-after <run.after@protonmail.com>
2019-12-23 20:16:25 -0600

    Node class created. build_tree method complete

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Node
   attr_accessor :left, :data, :right
   def initialize(data, left=nil, right=nil)
@@ -7,12 +8,36 @@ class Node
   end
 end
 
-def build_tree(array)
+def build_tree(array, start=0, last=0)
+  #binding.pry  
+  if start > last
+    return nil
+  end
   #sorts and removes duplicates
   array.sort!.uniq!
-
-  node = Node.new(array[array.size/2])
+  start = 0
+  last = array.size
+  mid = (start + last)/2
+  if array[mid] != nil #this line removes empty nodes
+    root = Node.new(array[mid])
+    left = array[start...mid]
+    right = array[mid + 1..last]
+  
+    root.left = build_tree(left, start, mid - 1)
+    root.right = build_tree(right, mid + 1, last)
+  end 
+  return root
 end
 
+#p build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
-p build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+=begin
+####### Fensus said to use ##########
+ def pretty_print(node = root, prefix="", is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? "│   " : "    "}", false) if node.right
+    puts "#{prefix}#{is_left ? "└── " : "┌── "}#{node.data.to_s}"
+    pretty_print(node.left, "#{prefix}#{is_left ? "    " : "│   "}", true) if node.left
+  end
+=end
+
