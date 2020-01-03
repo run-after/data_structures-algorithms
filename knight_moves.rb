a9a7458d5ade5891e468103fdaad0769c619fba7
run-after <run.after@protonmail.com>
2020-01-03 17:17:28 -0600

    added knight_moves.rib

---
+++ knight_moves.rb
@@ -0,0 +1,34 @@
+class Board
+  
+  def initialize
+
+
+
+  end
+
+  def draw
+
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+    puts "[ ][ ][ ][ ][ ][ ][ ][ ]"
+
+  end
+
+end
+
+class Knight
+
+  def initialize
+
+
+
+  end
+
+end
+
+Board.new.draw
